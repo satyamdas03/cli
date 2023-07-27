514b763597e83d06e2ee52fb489629e6210eab84
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-27 18:20:30 +0530

    insbdakj

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ import chalkAnimation from 'chalk-animation';
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 
-console.log('hello world');
+console.log(chalk.bgGreen('hello world'));
+
+let playerName;
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        "who wants to be a millionare? \n"
+    );
+}
